eee79da971387924993b876d9b42009740265c90
“kenkanayama” <ken.kanayama@pastrue.biz>
2023-02-21 00:32:20 +0900

    コンポーネントにpropsで値を渡す

--- react-guide-material/04_react_basic/src/090_props/start/Example.js
+++ react-guide-material/04_react_basic/src/090_props/start/Example.js
@@ -1,11 +1,24 @@
 import Child from "./components/Child";
 
 const Example = () => {
+    const hello = (arg) => `Hello ${arg}`;
+    const o = {
+        color: "blue",
+        num: 123
+    }
     return (
         <>
-        <Child color=""/>
-        <Child color="red"/>
-        <Child />
+            <Child
+                {...o}//スプレッド演算子
+                // color="red"
+                // num={123}
+                fn={hello}
+                bool
+                obj={{ name : "tom", age:18}}
+            />
+            {/* <Child color=""/>
+            <Child color="red"/>
+            <Child /> */}
         </>
     )
 };
--- react-guide-material/04_react_basic/src/100_practice_props/start/components/Profile.js
+++ react-guide-material/04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,9 +1,11 @@
 import "./Profile.css";
 
-const Profile = (props) => {
+const Profile = ({name, age = '??', country}) => {
   return (
     <div className="profile">
-
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
+      <h3>From: {country}</h3>
     </div>
   );
 };
