0cc7150ba1ec3d88ce8527007a5c08bc613d434c
“kenkanayama” <ken.kanayama@pastrue.biz>
2023-02-24 00:40:50 +0900

    JSXとは何か

--- react-guide-material/04_react_basic/src/120_props_rules/start/components/Hello.js
+++ react-guide-material/04_react_basic/src/120_props_rules/start/components/Hello.js
@@ -1,4 +1,7 @@
 const Hello = (props) => {
+  const desc = Reflect.getOwnPropertyDescriptor(props, 'name');
+  //これでオブジェクトの設定を確認することができる。
+  console.log(desc);
   return (
     <div>
       <h3>Hello {props.name}</h3>
