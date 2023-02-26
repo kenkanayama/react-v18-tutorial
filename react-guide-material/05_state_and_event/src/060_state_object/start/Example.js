85e337b224127245abf040c40d6de0c2075668e8
“kenkanayama” <ken.kanayama@pastrue.biz>
2023-02-26 23:02:26 +0900

    オブジェクト型のState更新

--- react-guide-material/05_state_and_event/src/060_state_object/start/Example.js
+++ react-guide-material/05_state_and_event/src/060_state_object/start/Example.js
@@ -2,6 +2,28 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj)
+  const changeName = (e) => {
+    setPerson({ name: e.target.value, age: person.age})
+  };
+  const changeAge = (e) => {
+    //setPerson({ name: person.name, age: e.target.value})
+    setPerson({ ...person, name: e.target.value})
+  };
+  const reset = () => {
+    setPerson({ name: "", age: ""})
+  }
+  return (
+    <>
+      <h3>Name:{person.name}</h3>
+      <h3>Age:{person.age}</h3>
+      <input type="text" onChange={changeName} value={person.name}/>
+      <input type="number" onChange={changeAge} value={person.age}/>
+      <div>
+      <button onClick={reset}>リセット</button>
+      </div>
+    </>
+  );
 };
 
 export default Example;
