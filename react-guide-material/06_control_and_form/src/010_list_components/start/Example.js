d6d0543f1cfb5cd656a46eb022db13bf51cae5b3
“kenkanayama” <ken.kanayama@pastrue.biz>
2023-03-01 00:00:23 +0900

    keyについてメモ

--- react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
+++ react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
@@ -5,18 +5,18 @@ const Example = () => {
 
   const animalList = [];
   for(const animal of animals){
-    animalList.push(<li>{animal}</li>)
+    animalList.push(<li key={animal}>{animal}</li>)
   }
 
-  const animalMap = animals.map((animal) => <li>Hello, {animal}</li>)
+  const animalMap = animals.map((animal) => <li key={animal}>Hello, {animal}</li>)
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-      {animalList}
-      {animalMap}
-      {animals.map((animal) => <li>HelloMap, {animal}</li>)}
+      {/* {animalList}
+      {animalMap} */}
+      {animals.map((animal) => <li key={animal}>HelloMap, {animal}</li>)}
       </ul>
     </>
   );
