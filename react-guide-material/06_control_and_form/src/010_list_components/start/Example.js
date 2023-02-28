783220b293d60b5f3b94982b3f2cab1dbbfe8c66
“kenkanayama” <ken.kanayama@pastrue.biz>
2023-02-28 09:16:46 +0900

    制御構文とフォームの制御のレクチャーに突入

--- react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
+++ react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
@@ -3,11 +3,20 @@ const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
 
+  const animalList = [];
+  for(const animal of animals){
+    animalList.push(<li>{animal}</li>)
+  }
+
+  const animalMap = animals.map((animal) => <li>Hello, {animal}</li>)
+
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+      {animalList}
+      {animalMap}
+      {animals.map((animal) => <li>HelloMap, {animal}</li>)}
       </ul>
     </>
   );
