0fdce87a65d96192bf1119c174dec6918403aa13
“kenkanayama” <ken.kanayama@pastrue.biz>
2023-03-04 22:12:42 +0900

    複数チェックボックスの実装とチェックごとに結果を変更する

--- react-guide-material/06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ react-guide-material/06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -9,9 +9,49 @@ const Example = () => {
 
   const [sum, setSum] = useState(0);
 
+  const hadleChange = (e) => {
+    const newFruits = fruits.map(fruit => {
+      const newFruit = {...fruit }
+      if(newFruit.label === e.target.value){
+        newFruit.checked = !fruit.checked
+      }
+      return newFruit;
+    })
+    setFruits(newFruits);
+
+    let sumVla = 0;
+    // newFruits.forEach(fruit => {
+    //   if(fruit.checked){
+    //     sumVla += fruit.value
+    //   }
+    // });
+
+    //以下のパターンもある
+    newFruits
+    .filter(fruit => fruit.checked)
+    .forEach(fruit => {sumVla += fruit.value});
+
+    setSum(sumVla)
+  };
+
   return (
     <div>
-      
+      {fruits.map(fruit => {
+        return (
+          <div key={fruit.label}>
+            <input
+              type="checkbox"
+              id={fruit.label}
+              value={fruit.label}
+              checked={fruit.checked}
+              onChange={hadleChange}
+            />
+            <label htmlFor={fruit.label}>
+              {fruit.label}:{fruit.value}
+            </label>
+          </div>
+        )
+      })}
       <div>合計：{sum}</div>
     </div>
   );
